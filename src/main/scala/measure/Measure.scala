ab56644e283eea88dc13d88b2ffbf3828f88e52e
Алексей <wert@laptop.(none)>
2013-05-16 14:30:38 +0700

    initial

---
+++ src/main/scala/measure/Measure.scala
@@ -0,0 +1,25 @@
+package measure
+
+object Measure {
+
+	/**
+	 * Prints how long given block of code was executed and return it result
+	 */
+	def printTime[A](a: => A) = {
+    	val start = System.nanoTime
+    	val result = a
+    	val micros = (System.nanoTime - start) / 1000
+    	println(micros + " microseconds")
+    	result
+	}
+
+	/**
+	 * Returns a tuple with block execution result and execution time in microseconds
+	 */
+	def getTime[A](a: => A): (A, Long) = {
+		val start = System.nanoTime
+		val result = a
+		val micros = (System.nanoTime - start) / 1000
+		(result, micros)
+	}
+}
